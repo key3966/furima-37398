46c2507dd569d1b1dbb6031908bd8c902647f043
key3966 <ev0.kpx@gmail.com>
2022-02-01 12:48:39 +0900

    itemモデルにバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,20 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_time
+
+  with_options presense: true do
+    validates :title
+    validates :explanation
+    validates :price
+    validates :user
+  end
+  
+  with_options numericality: { other_than: 1 , message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_time_id
+  end
+
 end
