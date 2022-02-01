62568b1dd6c885c5d8ac8dd74471ddacfc762e4f
key3966 <ev0.kpx@gmail.com>
2022-02-01 15:24:20 +0900

    itemコントローラーのnew,createアクションにインスタンス変数とストロングパラメーターを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,20 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.find(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :explanation, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :shipping_time_id).merge(user_id: current_user.id)
   end
 end
