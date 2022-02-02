42db56ff488dba29729eb97068309290f9ef63ae
key3966 <ev0.kpx@gmail.com>
2022-02-02 12:12:30 +0900

    itemのFactorybot設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    title            { Faker::Lorem.word }
+    explanation      { Faker::Lorem.paragraph }
+    category_id      { Faker::Number.between(from: 2, to: 11) }
+    status_id        { Faker::Number.between(from: 2, to: 7) }
+    shipping_fee_id  { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id    { Faker::Number.between(from: 2, to: 48) }
+    shipping_time_id { Faker::Number.between(from: 2, to: 4) }
+    price            { Faker::Number.between(from: 300, to: 9999999) }
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
