aef01a8b16b8dc2c370aa23c46d7ddc9172ffce0
key3966 <ev0.kpx@gmail.com>
2022-02-04 14:41:49 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
+  
   def index
     @order_address = OrderAddress.new
     redirect_to root_path if @item.user_id == current_user.id || @item.order.present?
