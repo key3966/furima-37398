1a805084165156f82df73c94b2e342131fb627bd
key3966 <ev0.kpx@gmail.com>
2022-02-04 11:04:59 +0900

    テストコード記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,9 +4,7 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
-    if @item.user_id == current_user.id || @item.order
-      redirect_to root_path
-    end
+    redirect_to root_path if @item.user_id == current_user.id || @item.order
   end
 
   def new
@@ -23,15 +21,17 @@ class OrdersController < ApplicationController
       render :index
     end
   end
-  
+
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_nam, :tel).merge(user_id: current_user.id, item_id: @item.id, price: @item.price, token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_nam, :tel).merge(
+      user_id: current_user.id, item_id: @item.id, price: @item.price, token: params[:token]
+    )
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: order_params[:price],
       card: order_params[:token],
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,7 +5,7 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :city
     validates :house_number
     validates :tel, numericality: { only_integer: true }, length: { in: 10..11 }
@@ -16,7 +16,7 @@ class OrderAddress
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, tel: tel, order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number,
+                   building: building, tel: tel, order_id: order.id)
   end
-
 end
